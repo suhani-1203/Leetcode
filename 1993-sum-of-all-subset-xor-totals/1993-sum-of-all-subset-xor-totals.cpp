2ce4aa9684aebe9953f482b156e588a4c97016b6
suhani-1203 <sagarwal1_be22@thapar.edu>
2025-05-03 14:45:58 +0530

    Time: 0 ms (100%), Space: 9.3 MB (34.84%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
@@ -1,24 +1,10 @@
 class Solution {
 public:
-int sum=0;
-    void generateSubsets(vector<int>& nums,vector<int>& temp,int index){
-        if(index==nums.size()){
-            int xor_elements=0;
-            for(int x:temp){
-                xor_elements^=x;
-            }
-            sum+=xor_elements;
-            return;
-        }
-        generateSubsets(nums,temp,index+1);
-        temp.push_back(nums[index]);
-        generateSubsets(nums,temp,index+1);
-        temp.pop_back();
-        return;
-    }
     int subsetXORSum(vector<int>& nums) {
-        vector<int> temp;
-        generateSubsets(nums,temp,0);
-        return sum;
+        int total = 0;
+        for (int num : nums) {
+            total |= num;  // Step 1: Compute bitwise OR of all numbers
+        }
+        return total * (1 << (nums.size() - 1));  // Step 2: Multiply by 2^(n-1)
     }
 };
