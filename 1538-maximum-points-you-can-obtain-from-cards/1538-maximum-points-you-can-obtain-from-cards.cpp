2dc0c05409bb571a6019cd223ca35e83384cf136
suhani-1203 <sagarwal1_be22@thapar.edu>
2025-05-14 00:08:32 +0530

    Time: 0 ms (100%), Space: 46.3 MB (13.53%) - LeetHub

--- 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,35 +1,19 @@
 class Solution {
 public:
         int maxScore(vector<int>& cardPoints, int k) {
-        int sum = 0;
-        int n = cardPoints.size();
-        
-        vector<int> cummulativeSumFromFront(n+1, 0);
-        vector<int> cummulativeSumFromBehind(n+1, 0);
-        
-        sum = 0;
-        for (int i=0; i<n; i++) {
-            sum += cardPoints[i];
-            cummulativeSumFromFront[i+1] = sum;
+        int n=cardPoints.size();
+        int lsum=0,maxScore=0;
+        for(int i=0;i<k;i++){
+            lsum+=cardPoints[i];
         }
-        sum = 0;
-        for (int i=n-1; i>=0; i--) {
-            sum += cardPoints[i];
-            cummulativeSumFromBehind[i] = sum;
+        maxScore=lsum;
+        int rsum=0,rindex=n-1;
+        for(int i=k-1;i>=0;i--){
+            lsum-=cardPoints[i];
+            rsum+=cardPoints[rindex];
+            rindex--;
+            maxScore=max(maxScore,lsum+rsum);
         }
-        
-        // Reversing is optional. I reversed it so that it would be easy
-        // to access sum of last (k-i) elements by just indexing at [k-i]
-        // Otherwise, I would have had to index it at [n-k+i] which would
-        // have made it difficult to read.
-        reverse(cummulativeSumFromBehind.begin(), cummulativeSumFromBehind.end());
-        
-        int answer = 0;
-        for(int i=0; i<=k; i++) {      
-            answer = max(answer, 
-                           cummulativeSumFromFront[i] // Sum of first 'i' cards.
-                         + cummulativeSumFromBehind[k-i]); // Sum of last 'k-i' cards.
-        }
-        return answer;
+        return maxScore;
     }
 };
