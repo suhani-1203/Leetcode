29a8afc5ff8ccb97feba26ed62ff7cfc67ba6222
suhani-1203 <sagarwal1_be22@thapar.edu>
2025-05-16 01:02:53 +0530

    Time: 7 ms (70.8%), Space: 11.2 MB (24.78%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int count=0;
-        unordered_map<char,int> lastSeen={{'a',-1},{'b',-1},{'c',-1}};
-        for(int i=0;i<s.length();i++){
-            lastSeen[s[i]]=i;
-            if(lastSeen['a']!=-1 && lastSeen['b']!=-1 && lastSeen['c']!=-1)
-                count=count+1+min(lastSeen['a'],min(lastSeen['b'],lastSeen['c']));
+        int count = 0;
+        vector<int> lastSeen(3, -1); 
+
+        for (int i = 0; i < s.length(); i++) {
+            lastSeen[s[i] - 'a'] = i;
+
+            if (lastSeen[0] != -1 && lastSeen[1] != -1 && lastSeen[2] != -1) {
+                count += 1 + min({lastSeen[0], lastSeen[1], lastSeen[2]});
+            }
         }
+
         return count;
     }
 };
