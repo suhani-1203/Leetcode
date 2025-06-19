c01f46158918a0e1d64690efe2e7811008a19784
suhani-1203 <sagarwal1_be22@thapar.edu>
2025-06-19 08:41:10 +0530

    Time: 3 ms (56.17%), Space: 14.1 MB (73.37%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    void recurSubset(vector<vector<int>>& ans ,vector<int>& nums,vector<int>& temp,int index,int n, int k, int sum){
-    if(index==n){
-        if(sum==k)
-            ans.push_back(temp);
+    void recurSubset(vector<vector<int>>& ans ,vector<int>& nums,vector<int>& temp,int index,int n, int k){
+    if(k==0){
+        ans.push_back(temp);
         return;
     }
-    if(sum>k)
+    if(index==n)
         return;
-    if(nums[index]<=k){
-        temp.push_back(nums[index]);
-        recurSubset(ans,nums,temp,index,n,k-nums[index],sum);
+    for(int i=index;i<n;i++){
+        if(nums[i]>k)
+            continue;
+        temp.push_back(nums[i]);
+        recurSubset(ans,nums,temp,i,n,k-nums[i]);
         temp.pop_back();
     }
-
-    recurSubset(ans,nums,temp,index+1,n,k,sum);
     return;
 }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
         vector<int> temp;
-        recurSubset(ans,candidates,temp,0,candidates.size(),target,0);
+        recurSubset(ans,candidates,temp,0,candidates.size(),target);
         return ans;
     }
 };
