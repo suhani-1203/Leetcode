bcb2a1ffe524207191e7b362a99121a40f49c069
suhani-1203 <sagarwal1_be22@thapar.edu>
2025-06-28 12:29:54 +0530

    Time: 0 ms (100%), Space: 8.6 MB (17.87%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,6 +1,36 @@
 class Solution {
 public:
-    int climbStairs(int n) {
+    int climbStairs(int n){
+        if(n==1) return 1;
+        if(n==2) return 2;
+        vector<int> dp(n+1);
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*int climbStairs(int n) {
         if(n<=3)
             return n;
         int prev1=3;
@@ -12,6 +42,4 @@ public:
             prev2=prev1;
             prev1=curr;
         }
-        return curr;
-    }
-};
+        return curr;*/
