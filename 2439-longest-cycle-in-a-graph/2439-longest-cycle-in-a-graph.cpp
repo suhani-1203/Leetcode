c8457a0db382583df95c475abf63d5766e74019a
Suhani Agarwal <sagarwal1_be22@thapar.edu>
2025-07-16 22:42:31 +0530

    Time: 16 ms (94.77%), Space: 104.4 MB (78.98%) - LeetHub

--- 2439-longest-cycle-in-a-graph/2439-longest-cycle-in-a-graph.cpp
+++ 2439-longest-cycle-in-a-graph/2439-longest-cycle-in-a-graph.cpp
@@ -1,36 +1,42 @@
 class Solution {
 public:
-    int longestCycle(vector<int>& edges) {
-        int n = edges.size();
-        vector<int> visited(n, -1);
-        int maxCycle = -1;
-        int time = 0;
+    void dfs(int node, vector<int>& edges,
+             vector<int>& visNode,
+             vector<int>& timeInPath,
+             int currTime,
+             int& maxCycleLen) {
 
-        for (int i = 0; i < n; i++) {
-            if (visited[i] != -1)
-                continue;
+        visNode[node] = 1;
+        timeInPath[node] = currTime;
 
-            int curr = i;
-            unordered_map<int, int> pathTime;
-
-            while (curr != -1) {
-                if (visited[curr] != -1) {
-                    // Found a previously visited node
-                    if (pathTime.count(curr)) {
-                        int cycleLen = time - pathTime[curr];
-                        maxCycle = max(maxCycle, cycleLen);
-                    }
-                    break;
-                }
-
-                visited[curr] = time;
-                pathTime[curr] = time;
-                time++;
-                curr = edges[curr];
+        int it = edges[node];
+        if (it != -1) {
+            if (!visNode[it]) {
+                dfs(it, edges, visNode, timeInPath, currTime + 1, maxCycleLen);
+            }
+            else if (timeInPath[it] != -1) {
+                int cycleLen = currTime - timeInPath[it] + 1;
+                maxCycleLen = max(maxCycleLen, cycleLen);
             }
         }
 
-        return maxCycle;
+        timeInPath[node] = -1; // backtrack
+    }
+
+    int longestCycle(vector<int>& edges) {
+        int V = edges.size();
+
+        vector<int> visNode(V, 0);
+        vector<int> timeInPath(V, -1);
+        int maxCycleLen = -1;
+
+        for (int i = 0; i < V; i++) {
+            if (!visNode[i]) {
+                dfs(i, edges, visNode, timeInPath, 0, maxCycleLen);
+            }
+        }
+
+        return maxCycleLen;
     }
 };
 
