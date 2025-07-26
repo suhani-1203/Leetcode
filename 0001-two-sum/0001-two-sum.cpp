9cf152aebbe0b2259cbb0ac16c367948b70aaf28
Suhani Agarwal <sagarwal1_be22@thapar.edu>
2025-07-26 12:30:25 +0530

    Time: 0 ms (100%), Space: 14.8 MB (50.73%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,14 +2,17 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n=nums.size();
-        unordered_map<int,int> prefixSum;
+        unordered_map<int,int> numsIndex;
+        vector<int> indices;
         for(int i=0;i<n;i++){
-            int rem=target-nums[i];
-            if(prefixSum.count(rem)){
-                return{prefixSum[rem],i};
+            int complement=target-nums[i];
+            if(numsIndex.count(complement)){
+                indices.push_back(numsIndex[complement]);
+                indices.push_back(i);
+                break;
             }
-            prefixSum[nums[i]]=i;
+            numsIndex[nums[i]]=i;
         }
-        return {};
+        return indices;
     }
 };
