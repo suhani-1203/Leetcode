6abd920ead560ffd490360207c8e443ee3d5fbb5
Suhani Agarwal <sagarwal1_be22@thapar.edu>
2025-07-29 22:09:16 +0530

    Time: 0 ms (100%), Space: 8.9 MB (33.2%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     bool isValid(string s) {
-        if(s.length()==1)
-            return false;
-
         stack<char> st;
-        
-        for(auto it :s){
-            if(it=='(' || it=='{'||it=='[')
+        for(char it:s){
+            if(it=='(' || it=='{' || it=='['){
                 st.push(it);
+            }
             else{
-                if(st.empty())
-                    return false;
-                if((it==')' && st.top()!='(')||(it=='}' && st.top()!='{' )||(it==']' && st.top()!='['))
-                    return false;
+                if(st.empty()) return false;
+                if(it==')'&& st.top()!='(') return false;
+                if(it=='}'&& st.top()!='{') return false;
+                if(it==']'&& st.top()!='[') return false;
                 st.pop();
+        
             }
         }
         return st.empty();
